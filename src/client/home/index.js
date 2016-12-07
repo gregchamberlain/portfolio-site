77aaae68a789cc61d71d8c951d2d26f8b3731574
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-12-06 19:22:59 -0800

    Modified skill icon position

--- src/client/home/index.js
+++ src/client/home/index.js
@@ -16,10 +16,10 @@ import cssIcon from '../assets/icons/css.png';
 const Home = () => (
   <div className={styles.background}>
     <div className={styles.image} style={{ backgroundImage: `url(${profileSrc})`}}/>
-    <div className={styles.icon} style={{ backgroundImage: `url(${apolloIcon})`, top: '5%', left: '35%'}}/>
-    <div className={styles.icon} style={{ backgroundImage: `url(${reactIcon})`, top: '5%', right: '35%'}}/>
-    <div className={styles.icon} style={{ backgroundImage: `url(${reduxIcon})`, bottom: '5%', left: '35%'}}/>
-    <div className={styles.icon} style={{ backgroundImage: `url(${htmlIcon})`, bottom: '5%', right: '35%'}}/>
+    <div className={styles.icon} style={{ backgroundImage: `url(${apolloIcon})`, top: '10%', left: '35%'}}/>
+    <div className={styles.icon} style={{ backgroundImage: `url(${reactIcon})`, top: '10%', right: '35%'}}/>
+    <div className={styles.icon} style={{ backgroundImage: `url(${reduxIcon})`, bottom: '10%', left: '35%'}}/>
+    <div className={styles.icon} style={{ backgroundImage: `url(${htmlIcon})`, bottom: '10%', right: '35%'}}/>
     <div className={styles.icon} style={{ backgroundImage: `url(${nodeIcon})`, top: '25%', left: '20%'}}/>
     <div className={styles.icon} style={{ backgroundImage: `url(${graphqlIcon})`, top: '25%', right: '20%'}}/>
     <div className={styles.icon} style={{ backgroundImage: `url(${jsIcon})`, bottom: '25%', left: '20%'}}/>
