7fc82c32b3433bcf553e0998d404c496f09a11f6
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-02-05 13:44:15 -0800

    Added ordering to Home page Portfolio section

--- src/client/home/ProjectSection.js
+++ src/client/home/ProjectSection.js
@@ -13,4 +13,6 @@ const ProjectSection = ({ data }) => (
   </div>
 );
 
-export default graphql(projectsQuery)(ProjectSection);
+export default graphql(projectsQuery, {
+  options: { variables: { ids: ["584107bfef442d27fc77b0a5", "584214b14a19f110f154acbc", "584220eb50d408246fa673e2", "5848fb106652c06ba7174162", "5848d550e6c97c0011d6cd84", "584225d650d408246fa673e3", "5848c772efc3ff3d90a9e082", "584900fa6652c06ba7174163"] }}
+})(ProjectSection);
